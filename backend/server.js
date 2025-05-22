e9c2de6938653e10b0dc29a74a84f73442ecc249
giovanaassis <giovana_2011_11@hotmail.com>
2025-05-21 22:34:32 -0300

    feat: added rate limit with acrjet

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ import cors from "cors";
 import "dotenv/config";
 import productRoutes from "./routes/productRoutes.js";
 import { sql } from "./config/db.js";
+import { aj } from "./lib/arcjet.js";
+import { StatusCodes } from "http-status-codes";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -15,6 +17,48 @@ app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
 
+// APPLY AARCJET RATE-LIMIT TO ALL ROUTES
+app.use(async (req, res, next) => {
+  try {
+    const decision = await aj.protect(req, { requested: 1 });
+
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        res.status(StatusCodes.TOO_MANY_REQUESTS).json({
+          error: "Too many requests",
+        });
+      } else if (decision.reason.isBot()) {
+        res.status(StatusCodes.FORBIDDEN).json({
+          error: "Bot Acess Denied",
+        });
+      } else {
+        res.status(StatusCodes.FORBIDDEN).json({
+          error: "Forbidden",
+        });
+      }
+      return;
+    }
+
+    // CHECK FOR SPOOFED BOTS
+    if (
+      decision.results.some(
+        (result) => result.reason.isBot() && result.reason.isSpoofed()
+      )
+    ) {
+      res.status(StatusCodes.FORBIDDEN).json({
+        error: "Spoofed Bot Detected",
+      });
+      return;
+    }
+
+    next();
+  } catch (error) {
+    console.log("Arcjet Error: ", error);
+    next(error);
+  }
+});
+
+// ROUTES
 app.use("/api/products", productRoutes);
 
 const initDB = async () => {
