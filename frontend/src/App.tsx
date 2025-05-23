96594b026097603f9f8de9ad4fa04374c241ca5f
giovanaassis <giovana_2011_11@hotmail.com>
2025-05-22 23:33:29 -0300

    feat: initial config for frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import "./index.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-base-200 transition-colors duration-300 text-3xl">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product" element={<Product />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
